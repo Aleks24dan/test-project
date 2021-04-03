63464e50e17a573d1b2bea4a21375e01ed7a35cf
Aleks24dan <37366518+Aleks24dan@users.noreply.github.com>
2021-04-03 08:10:06 +0000

    Deploying to gh-pages from @ Aleks24dan/test-project@3a5940284150438c4ab3f837827cec5cc31351e3 🚀

--- src.11454aa7.js
+++ src.11454aa7.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/test-project/src.4886a0ad.js.map
+//# sourceMappingURL=/test-project/src.11454aa7.js.map
